516c63a0c49b8daaa4336999099885f8a16819d1
Webber <webber@takken.io>
2023-02-27 00:30:03 +0100

    chore: add coverage reporter

--- vitest.config.js
+++ vitest.config.js
@@ -10,5 +10,11 @@ export default defineConfig({
       bindings: {},
       kvNamespaces: ['TEST_NAMESPACE'],
     },
+    coverage: {
+      include: ['src'],
+      exclude: ['test', 'src/**/*.test.ts', 'src/**/*.spec.ts'],
+      provider: process.env.CI ? 'c8' : undefined,
+      reporter: process.env.CI ? ['text', 'json', 'html', 'lcov'] : undefined,
+    },
   },
 });
